9e9b287a913b707f24409d5ab0e9bedcd4a596d4
Ali Jazbi <ali.jazbi@yahoo.com>
2022-08-12 20:21:18 +0430

    add auto close function

---
+++ js/auto-close.js
@@ -0,0 +1,10 @@
+$( document ).ready(function() {
+   
+    $('.nav-link').click(function(){
+     $('.navbar div').removeClass('show');
+  });  
+  $('.single').click(function(){
+         $('.navbar-nav div').removeClass('show');
+  });
+ 
+   });   
